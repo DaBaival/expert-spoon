072605f25c96752544ecab09039accb7f01e468d
DaBaival <148878832+DaBaival@users.noreply.github.com>
2024-08-15 16:25:20 +0800

    Add files via upload

---
+++ 0318/B.py
@@ -0,0 +1,7 @@
+money=eval(input())
+m50=money//50
+m5=(money-m50*50)//5
+m1=money-m50*50-m5*5
+print("50yuan: ",m50)
+print("5yuan: ",m5)
+print("1yuan: ",m1)
---
+++ 0318/C.py
@@ -0,0 +1,16 @@
+a="Hello"
+b="python"
+print("a+b:",a+b)
+print("a*2:",a*2)
+print("a[1]:",a[1])
+print("a[1:4]:",a[1:4])
+if("H" in a):
+    print("H" in a)
+else:
+    print("H" not in a)
+if("M" not in a):
+    print("M" not in a)
+else:
+    print("M" in a)
+print(r'\n')
+print(R'\n')
